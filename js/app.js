8221dd166448e7e7c420dd481b65aaa2a9941628
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2024-03-03 13:03:29 -0500

    feat: :sparkles: Update functions

--- js/app.js
+++ js/app.js
@@ -1,22 +1,62 @@
-let tasks = [];
+let tasksPending = [];
+let tasksDone = [];
+let tasksFailed = [];
+const submitButton = document.querySelector('.creation__button');
 const tasksList = document.querySelector('.management');
 const listDone = document.querySelector('.list__done');
 const listFailed = document.querySelector('.list__failed');
 
+const deleteTask = (id) => {
+    tasksPending = tasksPending.filter(function(task) {
+        return task.id !== id;
+    });
+}
+
 const confirm = (id) => {
+    const task = tasksPending.find(task => task.id == id);
+    deleteTask(id);
+    updatePendingList(tasksPending);
+    task.id = tasksDone.length + 1;
+    tasksDone.push(task);
     listDone.innerHTML += `
-    <div class="management__task">
-        <p class="management__title">${id}</p>
+    <div class="list__task">
+        <p class="list__title">${task.task}</p>
     </div>
     `
+    console.log(tasksDone);
 }
 
 const discard = (id) => {
+    const task = tasksPending.find(task => task.id == id);
+    deleteTask(id);
+    updatePendingList(tasksPending);
+    task.id = tasksFailed.length + 1;
+    tasksFailed.push(task);
     listFailed.innerHTML += `
-    <div class="management__task">
-        <p class="management__title">${id}</p>
+    <div class="list__task">
+        <p class="list__title">${task.task}</p>
     </div>
     `
+    console.log(tasksFailed);
+}
+
+const updatePendingList = (listTasks) => {
+    tasksList.innerHTML = `<h1 class="titulo">Tareas Pendientes</h1>`;
+    for (taskElement of listTasks) {
+        tasksList.innerHTML += `
+        <div class="management__task">
+            <p class="management__title">${taskElement.task}</p>
+            <div class="management__buttons">
+                <button onclick="confirm(${taskElement.id})" class="management__button green" type="button">
+                    <i class='bx bx-check bx-sm'></i>
+                </button>
+                <button onclick="discard(${taskElement.id})" class="management__button red" type="button">
+                    <i class='bx bx-x bx-sm'></i>
+                </button>
+            </div>
+        </div>
+    `
+    }
 }
 
 document.querySelector('.creation__button').addEventListener('click', function (event) {
@@ -40,23 +80,10 @@ document.querySelector('.creation__button').addEventListener('click', function (
             }
         }
     }
-    tasksData['id'] = tasks.length + 1;
-    tasksData['state'] = 'pending';
-    tasks.push(tasksData);
+    tasksData['id'] = tasksPending.length + 1;
+    tasksPending.push(tasksData);
     form.reset();
     event.preventDefault();
-    tasksList.innerHTML += `
-        <div class="management__task">
-            <p class="management__title">${tasksData.task}</p>
-            <div class="management__buttons">
-                <button onclick="confirm(${tasksData.id})" class="management__button green" type="button">
-                    <i class='bx bx-check bx-sm'></i>
-                </button>
-                <button onclick="discard(${tasksData.id})" class="management__button red" type="button">
-                    <i class='bx bx-x bx-sm'></i>
-                </button>
-            </div>
-        </div>
-    `
-    // console.log(tasks);
+    updatePendingList(tasksPending);
+    console.log(tasksPending);
 });
